2aba2a23198b8a011e7edd2124de23e5123d3ace
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2022-04-10 02:39:58 +0300

    asdj

--- run.sh
+++ run.sh
@@ -6,18 +6,20 @@ TEMP_PATH="$(mktemp -d)"
 PATH="${TEMP_PATH}:$PATH"
 
 echo '::group::🐶 Installing reviewdog...'
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- 2>&1
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo '::endgroup::'
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo '::group:: Running autopep8 with reviewdog 🐶 ...'
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
 autopep8_exitcode="0"
 reviewdog_exitcode="0"
 
 autopep8_output="$(autopep8 -r -d ${INPUT_AUTOPEP8_FLAGS} "${INPUT_TARGET:-.}" 2>&1)" || autopep8_exitcode="$?"
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 echo "${autopep8_output}" | reviewdog -f=diff -reporter=github-pr-check || reviewdog_exitcode="$?"
 echo '::endgroup::'
 
