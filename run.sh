61b5bc89e0f8479e3fb73425d6d34e5d956834aa
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2022-04-10 03:15:32 +0300

    asdj

--- run.sh
+++ run.sh
@@ -20,7 +20,9 @@ reviewdog_exitcode="0"
 
 autopep8_output="$(autopep8 -r -d ${INPUT_AUTOPEP8_FLAGS} "${INPUT_TARGET:-.}" 2>&1)" || autopep8_exitcode="$?"
 
-echo "${autopep8_output}" | reviewdog -f=diff -reporter=github-pr-check || reviewdog_exitcode="$?"
+git add -A
+echo "git diff --staged" | reviewdog -f=diff -diff="git diff --staged" -reporter=github-pr-check || reviewdog_exitcode="$?"
+git reset
 echo '::endgroup::'
 
 # Throw error if an error occurred and fail_on_error is true
